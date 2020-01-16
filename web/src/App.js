a8fbf5277195058d3ce69170d3054aa6690e084d
Fulvio <fulviocanducci@hotmail.com>
2020-01-16 14:38:25 -0300

    fase 3 complete

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import DevItem from './componentes/DevItem';
+import DevForm from './componentes/DevForm';
+
+import './global.css';
+import './App.css';
+import './sidebar.css';
+import './main.css';
 
 function App() {
-  return <div className="App">oi super</div>;
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    loadDevs();
+  }, []);
+
+  async function loadDevs() {
+    const response = await api.get('/devs');
+    setDevs(response.data);
+  }
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
